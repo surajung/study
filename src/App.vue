b1c5245722ec914150772dd5d57ebc67a8a59849
surajung <hdoc79@gmail.com>
2020-07-28 23:34:34 +0900

    vue-cli

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,31 @@
 <template>
   <div>
-    <ResultComponent></ResultComponent>
+    <app-header 
+      v-bind:propsdata="str"
+      v-on:renew="renewStr"
+    ></app-header>
+    <result-component></result-component>
   </div>
 </template>
 
 <script>
-  import ResultComponent from './components/ResultComponent'
+import AppHeader from './components/AppHeader.vue'
+import ResultComponent from './components/ResultComponent.vue'
 
   export default {
+    data: function() {
+      return {
+        str: 'header'
+      }
+    },
     components: {
+      'app-header': AppHeader,
       ResultComponent,
     },
+    methods: {
+      renewStr: function() {
+        this.str = 'hi'
+      }
+    }
   }
 </script>
