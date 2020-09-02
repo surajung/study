15335355b3e965a914fbd0e174265344151a1db7
surajung <surajung@saramin.co.kr>
2020-09-02 12:41:34 +0900

    7주차 퀴즈풀이

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+        "es2020": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 11,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+};
